02417d30eb516862a1a475ceb2ef949f45636735
Nelson Reina (@nelreina) <nelreina@gmail.com>
2018-06-27 16:15:02 -0400

    added script serve to start server/ don not start without PORT and API env vars

--- server.js
+++ server.js
@@ -9,8 +9,13 @@ const app = express();
 
 Log4js.configure("./log4js.json");
 const logger = Log4js.getLogger("app");
+
 const PORT = process.env.PORT;
 const API = process.env.API;
+if (!PORT || !API) {
+  logger.error("Env variables PORT and API are required to start the server!");
+  process.exit(0);
+}
 
 app.use(Log4js.connectLogger(logger, { level: "debug" }));
 app.use("/", express.static(path.join(__dirname, "apps/app1")));
@@ -27,6 +32,4 @@ app.get("*", (req, res) => res.sendFile(path.join(__dirname, "./404.html")));
 
 app.listen(PORT);
 
-logger.info(
-  `App ${name} version ${version} is running on port ${PORT} connected to API  ${API}`
-);
+logger.info(`${name}-${version}: started on port ${PORT}/ API ${API}`);
