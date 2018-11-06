c54d9f4de845544e53aa09ea0e3d7447868fe193
Nelson Reina (@nelreina) <nelreina@gmail.com>
2018-11-06 15:33:46 -0400

    make use of http-proxy-middleware iso request for proxy api and web socket

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const request = require("request");
 const Log4js = require("log4js");
+const proxy = require("http-proxy-middleware");
 
 const { name, version } = require("./package.json");
 const app = express();
@@ -10,8 +10,9 @@ const app = express();
 Log4js.configure("./log4js.json");
 const logger = Log4js.getLogger("app");
 
-const PORT = process.env.PORT;
-const API = process.env.API;
+const PORT = process.env.WEB_PORT;
+const API = `http://${process.env.API_HOST}:${process.env.API_PORT}`;
+const WS = `ws://${process.env.API_HOST}:${process.env.API_PORT}`;
 if (!PORT || !API) {
   logger.error("Env variables PORT and API are required to start the server!");
   process.exit(0);
@@ -21,12 +22,8 @@ app.use(Log4js.connectLogger(logger, { level: "debug" }));
 app.use("/", express.static(path.join(__dirname, "apps/app1")));
 app.use("/app2", express.static(path.join(__dirname, "apps/app2")));
 
-app.get("/api/*", (req, res) =>
-  req.pipe(request.get(`${API}${req.originalUrl}`)).pipe(res)
-);
-app.post("/api/*", (req, res) =>
-  req.pipe(request.post(`${API}${req.originalUrl}`)).pipe(res)
-);
+app.use("/api", proxy(`${API}/api`));
+app.use("/socket.io", proxy(`${WS}`, { ws: true }));
 
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "./404.html")));
 
